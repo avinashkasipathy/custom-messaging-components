cf9bea9d8b64b9d02c9a5d522a7f6b15b1e49214
avinashkasipathy <akasipathy@salesforce.com>
2025-04-17 17:11:54 -0700

    Initial commit with custom messaging components

--- messaging-components/force-app/main/default/lwc/commerceDynamicContentTextRenderer/commerceDynamicContentTextRenderer.js
+++ messaging-components/force-app/main/default/lwc/commerceDynamicContentTextRenderer/commerceDynamicContentTextRenderer.js
@@ -8,39 +8,56 @@ const PARTICIPANT_TYPES = [ENDUSER, AGENT, CHATBOT];
 
 export default class CommerceDynamicContentTextRenderer extends LightningElement {
   @api configuration;
-  @api conversationEntry;
+
+  _conversationEntry;
+  @api
+  set conversationEntry(value) {
+    this._conversationEntry = value;
+    this.processEntryPayload();
+  }
+  get conversationEntry() {
+    return this._conversationEntry;
+  }
 
   contentType = '';
   productData = [];
   entryPayload = {};
+  staticText;
+  
+  processEntryPayload() {
+    this.contentType = '';
+    this.productData = [];
+    this.entryPayload = {};
+    this.staticText = undefined;
+    this.parsedText = '';
 
-  connectedCallback() {
     try {
-      let rawPayload = this.conversationEntry?.entryPayload;
+      const rawPayload = this._conversationEntry?.entryPayload;
+
       try {
         this.entryPayload = JSON.parse(rawPayload);
       } catch {
-        // Not valid JSON, use as plain text
-        this.entryPayload = { abstractMessage: { staticContent: { text: rawPayload } } };
+        this.entryPayload = {
+          abstractMessage: {
+            staticContent: {
+              text: rawPayload
+            }
+          }
+        };
       }
-  
+
       this.staticText = this.entryPayload?.abstractMessage?.staticContent;
 
-      let parsedText;
-      if (typeof this.staticText?.text === "string") {
-        try {
-          parsedText = JSON.parse(this.staticText.text);
-        } catch (e) {
-          parsedText = this.staticText.text; // fallback to plain string
-        }
+      if (typeof this.staticText?.text === "string" && this.staticText?.text.includes("contentType")) {
+        this.parsedText = JSON.parse(this.staticText.text);
       } else {
-        parsedText = this.staticText?.text;
+        this.parsedText = this.staticText?.text;
       }
-  
-      this.contentType = parsedText?.contentType || '';
-  
-      if (this.isProductRecommendations && parsedText?.products) {
-        this.productData = parsedText.products;
+
+      this.contentType = this.parsedText?.contentType || '';
+
+      if (this.isProductRecommendations && this.parsedText?.products) {
+        this.productData = this.parsedText.products;
       }
     } catch (error) {
       console.error('Failed to process entryPayload:', error);
@@ -48,7 +65,7 @@ export default class CommerceDynamicContentTextRenderer extends LightningElement
   }
 
   get sender() {
-    return this.conversationEntry?.sender?.role;
+    return this._conversationEntry?.sender?.role;
   }
 
   get isProductRecommendations() {
@@ -56,8 +73,12 @@ export default class CommerceDynamicContentTextRenderer extends LightningElement
   }
 
   handleAddToCart(event) {
-    const product = event.detail.product.name;
-    this.configuration.util.sendTextMessage(`Can you help me add ${product} in 'Cardinal Red & Black' and size 'S' to cart`);
+    const product = event?.detail?.product?.name;
+    if (product) {
+      this.configuration.util.sendTextMessage(
+        `Can you help me add ${product} in 'Cardinal Red & Black' and size 'S' to cart`
+      );
+    }
   }
 
   get generateMessageBubbleClassname() {
@@ -73,33 +94,6 @@ export default class CommerceDynamicContentTextRenderer extends LightningElement
   }
 
   get textContent() {
-    try {
-      const staticContent = this.entryPayload?.abstractMessage?.staticContent;
-
-      if (staticContent?.text) {
-        const text = staticContent.text;
-
-        return text.replace( // innerText or textContent
-          /(?:(?:ht|f)tp(?:s?)\:\/\/|~\/|\/)?(?:\w+:\w+@)?((?:(?:[-\w\d{1-3}]+\.)+(?:com|org|net|gov|mil|biz|info|mobi|name|aero|jobs|edu|co\.uk|ac\.uk|it|fr|tv|museum|asia|local|travel|[a-z]{2}))|((\b25[0-5]\b|\b[2][0-4][0-9]\b|\b[0-1]?[0-9]?[0-9]\b)(\.(\b25[0-5]\b|\b[2][0-4][0-9]\b|\b[0-1]?[0-9]?[0-9]\b)){3}))(?::[\d]{1,5})?(?:(?:(?:\/(?:[-\w~!$+|.,=]|%[a-f\d]{2})+)+|\/)+|\?|#)?(?:(?:\?(?:[-\w~!$+|.,*:]|%[a-f\d{2}])+=?(?:[-\w~!$+|.,*:=]|%[a-f\d]{2})*)(?:&(?:[-\w~!$+|.,*:]|%[a-f\d{2}])+=?(?:[-\w~!$+|.,*:=]|%[a-f\d]{2})*)*)*(?:#(?:[-\w~!$ |\/.,*:;=]|%[a-f\d]{2})*)?/g,
-          function(imgUrl) {
-              // Only switch out to specific shortened urls if the agent is the user.
-              if(this.sender === AGENT) {
-                  // If the url is a specific link, then return a custom shortened link.
-                  if(imgUrl === "https://www.test.com/specificLink" || imgUrl === "https://www.test.com/anotherSpecificLink") {
-                      return `<a target="_blank" href="${imgUrl}">View Link</a>`;
-                  }
-                  // Otherwise just shorten to a generic link "View Article".
-                  return `<a target="_blank" href="${imgUrl}">View Article</a>`;
-              }
-              return imgUrl;
-          }.bind(this)
-        );
-      }
-
-      return '';
-    } catch (e) {
-      console.error('Error building textContent:', e);
-      return '';
-    }
+    return this.parsedText;
   }
 }
